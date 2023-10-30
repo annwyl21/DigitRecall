cd4fabc944f667d34f2d3e6faccad5314d29d29a
Ellen Houghton <ellen.a.ash@gmail.com>
2023-10-30 14:57:33 +0000

    digit generator added

--- src/DigitRecall.java
+++ src/DigitRecall.java
@@ -10,7 +10,7 @@ public class DigitRecall {
         Scanner scan = new Scanner(System.in);
 
         // game counter
-        int counter = 2;
+        int counter = 0;
 
         // HashMap with increased timings
         HashMap<Integer, Integer> timings = new HashMap<>();
@@ -19,15 +19,15 @@ public class DigitRecall {
         timings.put(3, 250);
 
         Boolean gameContinue = true;
-        while (gameContinue == true) {
+        while (gameContinue == true && counter < 28) {
 
             counter++;
             int waitingtime = counter %3;
 
             //display and capture a random digit
-            int num = rand.nextInt(10) + 1;
-            String stringOfNums = "";
-            stringOfNums += num;
+            int multiplier = (counter/3)+1;
+            int range = 10;
+            int num = rand.nextInt(range*multiplier) +1;
 
             // print number to screen
             System.out.println(num);
@@ -42,10 +42,14 @@ public class DigitRecall {
 
             // determine result
             System.out.println("Enter digit");
+            // add result
             int recalled = scan.nextInt();
+            // convert into a string for comparison
             String stringOfRecalled = "";
             stringOfRecalled += recalled;
-            ScreenWait.waitLoop(250);
+
+
+            ScreenWait.waitLoop(500);
             ScreenWait.clearScreen();
 
             // boolean output
@@ -61,6 +65,7 @@ public class DigitRecall {
         System.exit(0);
 
     }
+
 }
 
 // NOTES
