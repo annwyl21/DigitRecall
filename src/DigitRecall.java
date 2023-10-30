81706eff72b337b4e6052a5ff475deafc57969ce
Ellen Houghton <ellen.a.ash@gmail.com>
2023-10-30 15:32:13 +0000

    put timing and range into separate methods for readbility

--- src/DigitRecall.java
+++ src/DigitRecall.java
@@ -3,6 +3,13 @@ import java.util.Scanner;
 import java.util.HashMap;
 
 public class DigitRecall {
+    private static final int MAX_ROUNDS = 28;
+    /*
+     * 'private' means this variable is accessible only within the class it is defined in.
+     * 'static' means this variable belongs to the class, not to any specific instance of the class.
+     * 'final' indicates that the value of this variable cannot be changed once assigned.
+     * 'MAX_ROUNDS' is a constant representing the maximum number of rounds in the game, set to 28.
+     */
     public static void main(String[] args){
         // instance of random class
         Random rand = new Random();
@@ -13,25 +20,22 @@ public class DigitRecall {
         int counter = 0;
 
         // HashMap with increased timings
-        HashMap<Integer, Integer> timings = new HashMap<>();
-        timings.put(1, 750);
-        timings.put(2, 500);
-        timings.put(3, 250);
+        HashMap<Integer, Integer> timings = initialiseTimings();
+        HashMap<Integer, Integer> range = initialiseRange();
 
         Boolean gameContinue = true;
-        while (gameContinue == true && counter < 28) {
+        while (gameContinue == true && counter < MAX_ROUNDS) {
 
             counter++;
-            int waitingtime = counter %3;
+            int timingIndex = counter %3;
 
             //display and capture a random digit
-            int multiplier = (counter/3)+1;
-            int range = 10;
-            int num = rand.nextInt(range*multiplier) +1;
+            int digits = (counter/3)+1;
+            int num = rand.nextInt(range.get(digits)) +1;
 
             // print number to screen
             System.out.println(num);
-            switch (waitingtime) {
+            switch (timingIndex) {
                 case 0 -> ScreenWait.waitLoop(timings.get(1));
                 case 1 -> ScreenWait.waitLoop(timings.get(2));
                 case 2 -> ScreenWait.waitLoop(timings.get(3));
@@ -44,18 +48,12 @@ public class DigitRecall {
             System.out.println("Enter digit");
             // add result
             int recalled = scan.nextInt();
-            // convert into a string for comparison
-            String stringOfRecalled = "";
-            stringOfRecalled += recalled;
-
 
             ScreenWait.waitLoop(500);
             ScreenWait.clearScreen();
 
             // boolean output
-            if (num == recalled){
-                System.out.println(num == recalled);
-            } else {
+            if (num != recalled){
                 gameContinue = false;
             }
 
@@ -64,6 +62,28 @@ public class DigitRecall {
         System.out.println("Thank you for playing");
         System.exit(0);
 
+    }
+
+    public static HashMap<Integer, Integer> initialiseTimings(){
+        HashMap<Integer, Integer> timings = new HashMap<>();
+        timings.put(1, 750);
+        timings.put(2, 500);
+        timings.put(3, 250);
+        return timings;
+    }
+
+    public static HashMap<Integer, Integer> initialiseRange(){
+        HashMap<Integer, Integer> range = new HashMap<>();
+        range.put(1, 10);
+        range.put(2, 100);
+        range.put(3, 1000);
+        range.put(4, 10000);
+        range.put(5, 100000);
+        range.put(6, 1000000);
+        range.put(7, 10000000);
+        range.put(8, 100000000);
+        range.put(9, 1000000000);
+        return range;
     }
 
 }
